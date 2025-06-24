39a419d3e4b07cb15a07d11a8676fe459194a81d
tonysouthgate <gate_the@hotmail.com>
2025-06-24 13:47:18 +0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,11 @@
 
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  return res.status(200).json({
+    success: true,
+    message: 'API is working!',
+    timestamp: new Date().toISOString(),
+    endpoint: 'test'
+  });
+}
