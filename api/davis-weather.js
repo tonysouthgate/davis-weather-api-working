53402db1ad1944df81437497d2b285eb090654b3
tonysouthgate <gate_the@hotmail.com>
2025-06-24 13:58:32 +0100

    Update davis-weather.js
    Add Davis Weather function

--- api/davis-weather.js
+++ api/davis-weather.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'application/json');
 
+  
   // Handle preflight request
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
